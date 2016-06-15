ca65441dcbff8ed23ecb71a75bc012ee8cbf102a
Bang Nguyen <bangnv77@gmail.com>
2016-06-15 15:48:23 -0700

    add some codes to fix the total turn and player score results

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // business logic
-var player1Score = 0;
-var player2Score = 0;
+// var player1Score = 0;
+// var player2Score = 0;
 
-// var player1Score = "0";
-// var player2Score = "0";
+var player1Score = "0";
+var player2Score = "0";
 
-var totalScore = 0;
+var turnTotal = "";
 // var player1Total = [];
 // var player2Total = [];
 
@@ -16,32 +16,36 @@ var diceRolls = function (roll) {
 
 // user interface logic
 $(document).ready(function() {
-  event.preventDefault();
 
   $("button#roll1").click(function() {
 
-    $("#rollResult").text("");
+    $("#rollResult").text(" ");
     rollResult = diceRolls();
-    $("#rollResult").append(rollResult + ", ");
-    if (rollResult != 1) {
-      var newTotalScore = parseInt(totalScore) + rollResult;
-      totalScore = newTotalScore;
-      $("turnResult").text(totalScore);
-    } else {
-      totalScore = 0;
-      $("turnResult").text(totalScore);
-      $("rollResult").text("You lose your turn");
-      $("player1Buttons").hide();
-      $("player2Buttons").show();
-    }
 
+    $("#rollResult").text(rollResult + ", ");
+    if (rollResult != 1) {
+      var newTurnTotal = turnTotal[0] + rollResult;
+      console.log("wake up!")
+
+      turnTotal = newTurnTotal;
+      $("turnResult").text(turnTotal);
+
+    } else {
+      turnTotal = 0;
+      $("#turnResult").text(turnTotal);
+      $("#player1Score").text(turnTotal);
+      $("#rollResult").text("You lose your turn");
+      $("#player1Buttons").hide();
+      $("#player2Buttons").show();
+      console.log("wake up!")
+    }
   });
 
   $("button#hold1").click(function () {
-    player1Score = totalScore + parseInt(player1Score);
-    totalScore = 0;
+    player1Score = turnTotal + player1Score; //parseInt(player1Score);
+    turnTotal = 0;
     if (player1Score >= 100) {
-      alert("Player 1 is the Winnder")
+      alert("Player 1 is the Winner")
     } else {
       ("#rollResult").text(" ");
       $("turnResult").text(" ");
@@ -51,5 +55,38 @@ $(document).ready(function() {
     }
   });
 
+  $("button#roll2").click(function() {
 
+    $("#rollResult").text(" ");
+    rollResult = diceRolls();
+    $("#rollResult").text(rollResult + ", ");
+    if (rollResult != 1) {
+      var newTurnTotal = turnTotal[0] + rollResult;
+      turnTotal = newTurnTotal;
+      $("turnResult").text(turnTotal);
+    } else {
+      turnTotal = 0;
+      $("#turnResult").text(turnTotal);
+      $("#player2Score").text(turnTotal);
+      $("#rollResult").text("You lose your turn");
+      $("#player2Buttons").hide();
+      $("#player1Buttons").show();
+    }
+  });
+
+  $("button#hold2").click(function () {
+    player2Score = turnTotal + player2Score; //parseInt(player2Score);
+    turnTotal = 0;
+    if (player2Score >= 100) {
+      alert("Player 2 is the Winner")
+    } else {
+      ("#rollResult").text(" ");
+      $("turnResult").text(" ");
+      $("player1").text(player1Score);
+      $("#player2Buttons").hide();
+      $("#player1Buttons").show();
+    }
+  });
+
+  event.preventDefault();
 });
